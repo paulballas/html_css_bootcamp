6670e04e434ac3d8e9ff1bd58d1a11b4cf288f2b
paulballas <paul.ballas@gmail.com>
2016-01-31 14:33:00 -0700

    stuff

--- web.js
+++ web.js
@@ -1,13 +1,6 @@
-var app = connect();
-var server = app.listen(port);
-var server = http.createServer(app);
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-
-app.use(function middleware1(req, res, next) {
-
-});
 
 app.use(express.logger());
 app.use('/', express.static(__dirname));
