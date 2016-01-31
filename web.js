ff3d06f151b892f1910509e0defb241cfcbb9098
paulballas <paul.ballas@gmail.com>
2016-01-31 14:27:02 -0700

    middleware

--- web.js
+++ web.js
@@ -1,17 +1,16 @@
 var app = connect();
+var server = app.listen(port);
+var server = http.createServer(app);
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var server = app.listen(port);
-var server = http.createServer(app);
+
+app.use(function middleware1(req, res, next) {
+
+});
 
 app.use(express.logger());
 app.use('/', express.static(__dirname));
 app.listen(port, function() {
   console.log('Server started on ' + port);
-});
-
-app.use(function middleware1(req, res, next) {
-  // middleware 1
-  next();
 });
