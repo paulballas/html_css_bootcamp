8df1a315f86a90b67cfab7929bfacf9a6a9de7ae
paulballas <paul.ballas@gmail.com>
2016-01-31 14:13:53 -0700

    added middleware

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+var app = connect();
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var server = app.listen(port);
+var server = http.createServer(app);
 
 app.use(express.logger());
 app.use('/', express.static(__dirname));
 app.listen(port, function() {
   console.log('Server started on ' + port);
+});
+
+app.use(function middleware1(req, res, next) {
+  // middleware 1
+  next();
 });
